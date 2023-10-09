d86ac26ff9e6f001fde0175cdadc4fba0db22a78
Dahee Park <dapark@cx2r6s8.42seoul.kr>
2023-10-10 02:48:26 +0900

    cpp06

--- CPP_module_06/ex01/main.cpp
+++ CPP_module_06/ex01/main.cpp
@@ -6,13 +6,10 @@ int main()
 	data->content = "dapark";
 
 	std::cout << "before: " << data->content << std::endl;
-
 	uintptr_t ptr = Serializer::serialize(data);
 	data = Serializer::deserialize(ptr);
-
 	std::cout << "after: " << data->content << std::endl;
 
 	delete data;
-
-	return 0;
+	return (0);
 }
