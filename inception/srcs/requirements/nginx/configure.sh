4a714c7d157732ff46b0e7abbf6714ffdfe36f6f
Da hee Park <daheepark@Daui-MacBookAir.local>
2023-11-29 15:35:32 +0900

    modify mariadb

--- inception/srcs/requirements/nginx/configure.sh
+++ inception/srcs/requirements/nginx/configure.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
 
 # Create a self-signed SSL certificate (for testing purposes)
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/nginx-selfsigned.key -out /etc/nginx/certs/nginx-selfsigned.crt -subj "/C=US/ST=State/L=City/O=Organization/CN=login.42.fr"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/ssl/certs/nginx-selfsigned.key \
+        -out /etc/ssl/certs/nginx-selfsigned.crt \
+        -subj "/C=US/ST=State/L=City/O=Organization/CN=dapark.42.fr"
 
 # Create DH parameters (for additional security)
-openssl dhparam -out /etc/nginx/certs/dhparam.pem 2048
-
-# NGINX configuration
-server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-
-    server_name dapark.42.fr;
-
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_certificate /etc/nginx/certs/nginx-selfsigned.crt;
-    ssl_certificate_key /etc/nginx/certs/nginx-selfsigned.key;
-
-    location ~ \.php {
-        fastcgi_pass wordpress:9000;
-        fastcgi_split_path_info ^(.+\.php)(.*)$;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
-        include fastcgi_params;
-    }
-}
-
-# Start NGINX
-nginx -g "daemon off;"
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
